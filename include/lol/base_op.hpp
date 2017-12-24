3d3a3f4c06449d66cdcbc21d809f06bccf54c985
Sahin Habermann <sahino1996@hotmail.de>
2017-12-24 01:43:41 +0100

    rang.hpp and LeagueClient trashbin var

--- include/lol/base_op.hpp
+++ include/lol/base_op.hpp
@@ -122,7 +122,8 @@ namespace lol {
     WssClient wss;
     HttpsClient https;
     HttpsClient httpsa;
-    uint32_t id;
+	uint32_t id;
+	std::map<std::string, json> trashbin;
 
     LeagueClient(const LeagueClient&) = delete;
     LeagueClient(const std::string& address, int port, const std::string& password, uint32_t id = 0) :
